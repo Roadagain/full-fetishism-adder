e0311727beac1d66926799cae407f16062d648d0
Roadagain <ringoh72@gmail.com>
2021-01-17 02:03:17 +0900

    投稿された性癖をDynamoDBに保存する

--- backend/src/functions/post-fetishism/handler.ts
+++ backend/src/functions/post-fetishism/handler.ts
@@ -5,12 +5,32 @@ import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
+import { getDynamoDbClient } from '@libs/dynamodb';
+import { APIGatewayProxyResult } from 'aws-lambda';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Received your fetishism: "${event.body.fetishism}"`,
-    event,
-  });
+  const dynamoDbClient = getDynamoDbClient()
+
+  return new Promise<APIGatewayProxyResult>((resolve, reject) => {
+    dynamoDbClient.put({
+      TableName: 'fetishism',
+      Item: {
+        name: event.body.fetishism
+      },
+    }, (err, data) => {
+      if (err) {
+        console.error('error: ', err)
+        reject(err.message)
+        return
+      }
+      console.log('data: ', data)
+      resolve(formatJSONResponse({
+        message: `Received your fetishism: "${event.body.fetishism}"`,
+        event,
+      }))
+    })
+
+  })
 }
 
 export const main = middyfy(hello);
